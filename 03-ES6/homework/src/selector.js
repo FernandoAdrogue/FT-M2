19ff0d15fd782cf787ca6e7ab2c023621a5e64f6
FernandoAdrogue <chinoadrogue@gmail.com>
2023-04-05 22:18:42 -0300

    03-ES6\homework completed

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,12 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if(matchFunc(startEl))resultSet.push(startEl)
+  for(const child of startEl.children){
+    const result = traverseDomAndCollectElements(matchFunc,child)
+    resultSet = [...resultSet,...result]
+  }
+  return resultSet
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +23,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0]==='#')return 'id'
+  if(selector[0]==='.')return 'class'
+  if(selector.split('').slice(1).includes('.'))return 'tag.class'
+  return 'tag'
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,13 +38,29 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction = (element) => {
+      if(element.id === selector.slice(1)){
+        return true}
+      else return false
+    }
   } else if (selectorType === "class") {
-    
+    matchFunction = (element) => {
+      for(let elem of element.classList) {
+        if (elem === selector.slice(1))return true
+      }
+      return false
+    }
   } else if (selectorType === "tag.class") {
-    
+    matchFunction = (element) => {
+      [tag,classname] = selector.split('.')
+     return matchFunctionMaker(tag)(element) && matchFunctionMaker(`.${classname}`)(element)
+    }
   } else if (selectorType === "tag") {
-    
+    matchFunction = (element) => {
+      if(element.tagName === selector.toUpperCase()){
+        return true}
+      else return false
+    }
   }
   return matchFunction;
 };
@@ -47,3 +71,7 @@ var $ = function(selector) {
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
+
+// $('.clasecualquiera')
+// $('#idcialquiera')
+// $('p')
