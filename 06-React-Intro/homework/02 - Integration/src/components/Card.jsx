cf0519f6091e212460084f051c2fd6c368144a46
FernandoAdrogue <chinoadrogue@gmail.com>
2023-04-20 21:47:54 -0300

    homework 07-React Estilos

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,6 +1,6 @@
-export default function Card({id,name,status,species,gender,origin,image,onClose}) {
+export default function Card({name,status,species,gender,origin,image,onClose}) {
    return(
-      <div key={id}>
+      <div>
          <button onClick={onClose}>X</button>
          <h2>{name}</h2>
          <h2>{status}</h2>
--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -3,14 +3,15 @@ import Card from './Card';
 export default function Cards({characters}) {
    return ( 
       
-         characters.map(character => {
+         characters.map(({id,name,status,species,gender,origin,image}) => {
             return <Card
-                     name={character.name}
-                     status={character.status}
-                     species={character.species}
-                     gender={character.gender}
-                     origin={character.origin.name}
-                     image={character.image}
+                     key = {id}
+                     name={name}
+                     status={status}
+                     species={species}
+                     gender={gender}
+                     origin={origin.name}
+                     image={image}
                      onClose={() => window.alert('Emulamos que se cierra la card')}
                   />
             }
